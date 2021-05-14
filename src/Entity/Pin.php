45b913c55c559f56f9e42544551a0bdde2f7fc5f
ahthionkevin <ahthionkevin@gmail.com>
2021-05-14 10:20:52 +0000

    Add VitchUploaderBundle to project

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,10 +6,12 @@ use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\Traits\TimeStampable;
 use Symfony\Component\Validator\Constraints as Assert;
-
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
  * @ORM\HasLifecycleCallbacks
+ * @Vich\Uploadable
  */
 class Pin
 {
@@ -34,6 +36,18 @@ class Pin
      *  @Assert\Length(min=10)
      */
     private $description;
+
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="pin_image", fileNameProperty="imageName")
+     * 
+     * @var File|null
+     */
+    private $imageFile;
+
+
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -82,6 +96,24 @@ class Pin
         return $this;
     }
 
-   
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable());
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
     
 }
